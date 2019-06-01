faf75acbfd7c64488226b4c540fd37807450a76a
contactpeterchung.com <contactpeterchung@gmail.com>
2019-06-01 10:30:03 -0600

    changes made to app

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,28 +1,25 @@
 class AddressesController < ApplicationController
   before_action :set_address, only: [:show, :edit, :update, :destroy]
 
-  # GET /addresses
-  # GET /addresses.json
+  
   def index
     @addresses = Address.all
   end
 
-  # GET /addresses/1
-  # GET /addresses/1.json
+  
   def show
   end
 
-  # GET /addresses/new
+  
   def new
     @address = Address.new
   end
 
-  # GET /addresses/1/edit
+
   def edit
   end
 
-  # POST /addresses
-  # POST /addresses.json
+
   def create
     @address = Address.new(address_params)
 
@@ -37,8 +34,7 @@ class AddressesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /addresses/1
-  # PATCH/PUT /addresses/1.json
+  
   def update
     respond_to do |format|
       if @address.update(address_params)
@@ -51,8 +47,7 @@ class AddressesController < ApplicationController
     end
   end
 
-  # DELETE /addresses/1
-  # DELETE /addresses/1.json
+  
   def destroy
     @address.destroy
     respond_to do |format|
@@ -62,12 +57,10 @@ class AddressesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_address
       @address = Address.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def address_params
       params.require(:address).permit(:lat, :long, :country, :street, :city, :state, :zip, :location_id)
     end
---
+++ db/schema.rb
@@ -0,0 +1,56 @@
+ActiveRecord::Schema.define(version: 2019_06_01_161426) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "lat"
+    t.string "long"
+    t.string "country"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.bigint "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_addresses_on_location_id"
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "location_name"
+    t.integer "days"
+    t.text "activity"
+    t.bigint "trip_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trip_id"], name: "index_locations_on_trip_id"
+  end
+
+  create_table "trips", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trips_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "addresses", "locations"
+  add_foreign_key "locations", "trips"
+  add_foreign_key "trips", "users"
+end
